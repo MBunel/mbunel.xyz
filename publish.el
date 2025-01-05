5bf3f187bc0d94f4de14d5663b1048c2ddfff864
Mattia Bunel <mattia.bunel@ehess.fr>
2025-01-05 18:28:26 +0100

    Update title

--- publish.el
+++ publish.el
@@ -42,7 +42,7 @@
  :theme nil
  :base-url weblorg-default-url 
  :template-vars '(("site_lang" . "fr-fr")
-		  ("site_name" . "mbunel.xyz")
+		  ("site_name" . "MBunel.xyz")
                   ("site_owner" . "Mattia Bunel")
                   ("site_description" . "Mattia Bunel's personal website.")
 		  ("site_html_prefix" . "foaf: http://xmlns.com/foaf/0.1/ org: http://www.w3.org/ns/org#")))
