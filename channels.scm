9c590f0e02964154028d1e4a4dfbfec96b97d61a
Mattia Bunel <mattia.bunel@ehess.fr>
2025-09-15 17:36:19 +0200

    Revert "Update channel"
    This reverts commit 82e393bba485ced70cdb618f3d926dee466696b6.

--- channels.scm
+++ channels.scm
@@ -3,7 +3,7 @@
         (url "https://git.guix.gnu.org/guix.git")
         (branch "master")
         (commit
-          "de41613d273106b88eeed4af72c23a57cfd18442")
+          "323af5817a0d45ef5553da03c40948080aa7c48f")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
