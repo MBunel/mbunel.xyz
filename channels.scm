82e393bba485ced70cdb618f3d926dee466696b6
Mattia Bunel <mattia.bunel@ehess.fr>
2025-09-15 16:04:47 +0200

    Update channel

--- channels.scm
+++ channels.scm
@@ -1,9 +1,9 @@
 (list (channel
         (name 'guix)
-        (url "https://git.savannah.gnu.org/git/guix.git")
+        (url "https://git.guix.gnu.org/guix.git")
         (branch "master")
         (commit
-          "323af5817a0d45ef5553da03c40948080aa7c48f")
+          "de41613d273106b88eeed4af72c23a57cfd18442")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
--- manifest.scm
+++ manifest.scm
@@ -1,2 +1,2 @@
 (specifications->manifest
- '("bash" "make" "pandoc" "tidy" "emacs-no-x" "emacs-weblorg" "emacs-htmlize" "emacs-citeproc-el" "pdf2svg" "texlive-base" "texlive-standalone" "texlive-pgf" "texlive-pgfplots" "texlive-xcolor" "graphviz-minimal"))
+ '("bash" "make" "pandoc" "tidy-html" "emacs-no-x" "emacs-weblorg" "emacs-htmlize" "emacs-citeproc" "pdf2svg" "texlive" "texlive-pgf" "texlive-pgfplots" "texlive-xcolor" "graphviz-minimal"))
